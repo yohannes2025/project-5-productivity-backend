078f5eb996746da00080da4ce4f1102a64ffbe44
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-08 08:44:53 +0200

    Add Heroku domain to ALLOWED_HOSTS

--- env.py
+++ env.py
@@ -4,11 +4,11 @@ os.environ['CLOUDINARY_URL'] = "cloudinary://641477698697777:wBLMQwXop1dNYwX9Pg9
 
 os.environ['DATABASE_URL'] = "postgresql://neondb_owner:npg_ZM1uJgwao5Qf@ep-round-violet-a2uhsauy.eu-central-1.aws.neon.tech/stank_quake_sniff_187384"
 
-os.environ['SECRET_KEY'] = "django-insecure-ztits4*i8ygp0%ou9@s)p0f(5-%g56e01$=xt)0!_yho+c7=n4"
+# os.environ['SECRET_KEY'] = "django-insecure-ztits4*i8ygp0%ou9@s)p0f(5-%g56e01$=xt)0!_yho+c7=n4"
 
-# os.environ['SECRET_KEY'] = "+I1z*`08P|2XG6H.^QaAPrA"v#0l4CN|p}^#%fHeP9r_Euf7C)`?Pm[T:1c"%6j"
+os.environ['SECRET_KEY'] = "Yl;H@u1Rdew}O{x08~^*<&(QfLMM'/^DB~[juEb_L~>!S8SeSmd6dSe|Il^s&T1"
 
 os.environ['DEV'] = '1'
-os.environ['DEBUG'] = '1'
+# os.environ['DEBUG'] = '1'
 
 # os.environ['SECRET_KEY'] = "+I1z*`08P|2XG6H.^QaAPrA"v#0l4CN|p}^#%fHeP9r_Euf7C)`?Pm[T:1c"%6j"
