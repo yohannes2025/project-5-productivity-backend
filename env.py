32514f060e9f0c6b66e63aa5eb30256cbbc08ac1
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-08 10:06:28 +0200

    Add DATABASE_URL to env.py for deployment

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import os
 
 os.environ['CLOUDINARY_URL'] = "cloudinary://641477698697777:wBLMQwXop1dNYwX9Pg9Dd5jd2As"
 
-os.environ['DATABASE_URL'] = "postgresql://neondb_owner:npg_ZM1uJgwao5Qf@ep-round-violet-a2uhsauy.eu-central-1.aws.neon.tech/stank_quake_sniff_187384"
+os.environ['DATABASE_URL'] = "postgresql://neondb_owner:npg_ZM1uJgwao5Qf@ep-round-violet-a2uhsauy.eu-central-1.aws.neon.tech/slang_darn_stood_367172"
 
 # os.environ['SECRET_KEY'] = "django-insecure-ztits4*i8ygp0%ou9@s)p0f(5-%g56e01$=xt)0!_yho+c7=n4"
 
