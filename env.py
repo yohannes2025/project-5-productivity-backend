6024bb97c7f78f9c4cc0092da3c7c49414f2d5f6
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-08 17:45:22 +0200

    Update settings.py for Heroku deployment

--- env.py
+++ env.py
@@ -8,8 +8,7 @@ os.environ['DATABASE_URL'] = "postgresql://neondb_owner:npg_ZM1uJgwao5Qf@ep-roun
 
 os.environ['SECRET_KEY'] = "Yl;H@u1Rdew}O{x08~^*<&(QfLMM'/^DB~[juEb_L~>!S8SeSmd6dSe|Il^s&T1"
 
-os.environ['DEV'] = '1'
-# os.environ['DEBUG'] = '1'
+# os.environ['DEV'] = '1'
+os.environ['DEBUG'] = '1'
 
-# os.environ['SECRET_KEY'] = "+I1z*`08P|2XG6H.^QaAPrA"v#0l4CN|p}^#%fHeP9r_Euf7C)`?Pm[T:1c"%6j"
-os.environ['API_TOKEN'] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ2NzAzNDc3LCJpYXQiOjE3NDY3MDMxNzcsImp..."
+os.environ['ALLOWED_HOST'] = "127.0.0.1"
