0ee4694909477aaaf11a2fcb9894a98832fe0067
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-09 21:24:00 +0200

    Compile the static files

---
+++ staticfiles/precache-manifest.083a6a68a38bc14b832a44a290647f97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d718172b5d58156ab1c9265ac3e534f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb128fdf3778ce26ec9d",
+    "url": "/static/css/2.0ccbe742.chunk.css"
+  },
+  {
+    "revision": "da2a4a8efa62555c04a5",
+    "url": "/static/css/main.3126736c.chunk.css"
+  },
+  {
+    "revision": "cb128fdf3778ce26ec9d",
+    "url": "/static/js/2.b79dffbf.chunk.js"
+  },
+  {
+    "revision": "fb8c87fce1f6d24e64d4cbcfc86fa6d4",
+    "url": "/static/js/2.b79dffbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2a4a8efa62555c04a5",
+    "url": "/static/js/main.f9802a4e.chunk.js"
+  },
+  {
+    "revision": "b2c1873c7089132a7798",
+    "url": "/static/js/runtime-main.3242c491.js"
+  }
+]);
