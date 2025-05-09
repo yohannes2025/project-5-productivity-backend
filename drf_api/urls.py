d5c1d2479d8f7c76763234bf0d9735efc1ef0545
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-09 10:02:06 +0200

    Add the route to serve the index template

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,8 +1,16 @@
 # drf_api/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
-    path('', include('productivity_app.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-dj-auth/logout_route'),
+    path('api-dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api-dj-rest/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/, include('productivity_app.urls')),   
 ]
+
+handler404 = TemplateView.as_view(template_name='index.html')
