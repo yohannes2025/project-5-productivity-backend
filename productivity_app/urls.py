4be84a2bd2bc0537892c19819f39471f5ae05f68
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-09 10:02:06 +0200

    Add the route to serve the index template

--- productivity_app/urls.py
+++ productivity_app/urls.py
@@ -2,14 +2,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import LoginViewSet, TaskViewSet, ProfileViewSet, RegisterViewSet, UsersListAPIView
+from django.views.generic import TemplateView
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 router.register(r'profiles', ProfileViewSet, basename='profile')
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/register/', RegisterViewSet.as_view(), name='register'),
     path('api/login/', LoginViewSet.as_view(), name='login'),
     path('api/', include(router.urls)),
     path('api/users/', UsersListAPIView.as_view(), name='users-list'),
 ]
+
+handler404 = TemplateView.as_view(template_name='index.html')
