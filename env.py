effcea8b6b6ba006d1c06abdb9e4f1e2569c92c8
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-09 17:35:00 +0200

    Test the Build

--- env.py
+++ env.py
@@ -7,7 +7,7 @@ os.environ['DATABASE_URL'] = "postgresql://neondb_owner:npg_ZM1uJgwao5Qf@ep-roun
 os.environ['SECRET_KEY'] = "Yl;H@u1Rdew}O{x08~^*<&(QfLMM'/^DB~[juEb_L~>!S8SeSmd6dSe|Il^s&T1"
 
 # os.environ['DEV'] = '1'
-os.environ['DEBUG'] = '1'
+# os.environ['DEBUG'] = '1'
 
 os.environ['ALLOWED_HOST'] = "127.0.0.1"
 
