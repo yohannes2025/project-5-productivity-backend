2925c83e278b37a42bde064a91370fd113b6072a
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-10 12:36:48 +0200

    Remove env.py from version control

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,10 +1,10 @@
 # drf_api/urls.py
 from django.contrib import admin
 from django.urls import path, include
-# from django.views.generic import TemplateView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('', include('productivity_app.urls')),
     path('admin/', admin.site.urls),
     path('api/dj-rest-auth/registration/',
@@ -13,4 +13,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 ]
 
-# handler404 = TemplateView.as_view(template_name='index.html')
+handler404 = TemplateView.as_view(template_name='index.html')
