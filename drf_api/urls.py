a7a3e4a16c6e89a6773268383d4a28693f127c33
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-10 08:26:44 +0200

    Update settings.py and urls.py  for heroku deployment

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,24 +1,12 @@
 # drf_api/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    # path('api/dj-rest-auth/logout/', logout_route),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('productivity_app.urls')),
-    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('', include('productivity_app.urls')),
     path('api/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
-    path('api/', include('productivity_app.urls')),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
-
-handler404 = TemplateView.as_view(template_name='index.html')
-
-
-# path('api/register/', RegisterViewSet.as_view(), name='register'),
-# path('api/login/', LoginViewSet.as_view(), name='login'),
-# path('api/', include(router.urls)),
-# path('api/users/', UsersListAPIView.as_view(), name='users-list'),
