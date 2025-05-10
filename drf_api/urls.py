7921ecf9ce3df24741031e2f0a208347c5b7be88
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-10 10:57:37 +0200

    Update urls.py and settings.py for Heroku deployment

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,17 +1,16 @@
 # drf_api/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('api/', include('productivity_app.urls')),
+    # path('', TemplateView.as_view(template_name='index.html')),
+    path('', include('productivity_app.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('productivity_app.urls')),
     path('api/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
-handler404 = TemplateView.as_view(template_name='index.html')
+# handler404 = TemplateView.as_view(template_name='index.html')
