7c47a09fe39c66b6c6532e819360207b7a761771
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-17 17:33:21 +0200

    Use node 18

--- productivity_app/models.py
+++ productivity_app/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.utils import timezone
 from django.db import migrations, models
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 
 # ==========================
