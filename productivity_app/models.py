d45eb12c503fd9b51e1314ec3dad96c098276747
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-17 17:46:15 +0200

    Delete wrong import from models

--- productivity_app/models.py
+++ productivity_app/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.utils import timezone
-from django.db import migrations, models
 
 # ==========================
 # Task Management Models
