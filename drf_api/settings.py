e7e416c467864ca26c1fb7fad6a4abd52f9a2701
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-18 10:31:34 +0200

    Fix: modify CORS_ALLOWED_ORIGINS for network error fix

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -101,7 +101,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ORIGINS = True
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'access-control-allow-credentials',
